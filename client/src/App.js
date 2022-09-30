cf4c32b5822109b44722097e8fcc77b8a0fa2376
ajayvisu <ajay210481@gmail.com>
2022-09-30 12:44:16 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>BillSoft</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ server/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.listen(4000, () => {
+  console.log("server started");
+});
+
+app.get("/", (req, res) => {
+  res.send("BillSoft");
+});
