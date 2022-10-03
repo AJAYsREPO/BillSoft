b0531d9f55a0d6bdd07ea991b23414adf5654448
ajayvisu <ajay210481@gmail.com>
2022-10-03 13:05:13 +0530

    home component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/home/Home";
 
 function App() {
   return (
-    <>
-    <h1>BillSoft</h1>
-    </>
+    <Router>
+      <h1>BillSoft</h1>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ client/src/components/home/Home.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <>
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6 col-12 offset-md-3 ">
+            <div className="row gy-3">
+              <div className="col-md-6 col-12">
+                <button className="btn firstbtn1 w-100 py-5">Sales</button>
+              </div>
+              <div className="col-md-6 col-12">
+                <button className="btn firstbtn2 w-100 py-5">Admin</button>
+              </div>
+              <div className="col-md-6 col-12">
+                <button className="btn firstbtn1 w-100 py-5">future1</button>
+              </div>
+              <div className="col-md-6 col-12">
+                <button className="btn firstbtn2 w-100 py-5">future2</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
